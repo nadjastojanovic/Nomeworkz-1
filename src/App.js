eae18f6f673a377da7824ebd9526d1222a3658f7
NamanZelawat <namanzelawat@gmail.com>
2020-11-26 19:13:55 +0530

    Form

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Profile from "./components/Profile";
 import Hire from "./components/Hire";
 import { Provider } from "react-redux";
 import configureStore from "./redux/configureStore";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 
 import './App.css';
 
@@ -17,17 +19,19 @@ function App() {
   return (
   	<Provider store={store}>
 	  	<BrowserRouter>
-		    <div className="App">
-		    	<Switch>
-		    		<Route exact path="/" component={() => <Home />} />
-		    		<Route exact path="/signup" component={() => <Signup />} />
-		    		<Route exact path="/login" component={() => <Login />} />
-		    		<Route exact path="/reset" component={() => <Reset />} />
-		    		<Route exact path="/profile" component={() => <Profile />} />
-		    		<Route exact path="/hire" component={() => <Hire />} />
-		    		<Redirect to="/"/>
-		    	</Switch>
-		    </div>
+	  		<MuiThemeProvider>
+			    <div className="App">
+			    	<Switch>
+			    		<Route exact path="/" component={() => <Home />} />
+			    		<Route exact path="/signup" component={() => <Signup />} />
+			    		<Route exact path="/login" component={() => <Login />} />
+			    		<Route exact path="/reset" component={() => <Reset />} />
+			    		<Route exact path="/profile" component={() => <Profile />} />
+			    		<Route exact path="/hire" component={() => <Hire />} />
+			    		<Redirect to="/"/>
+			    	</Switch>
+			    </div>
+			</MuiThemeProvider>
 	    </BrowserRouter>
 	</Provider>
   );
