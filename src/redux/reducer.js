5b4a90805021698a7a0a9825cf7c51129f6d2e5c
NamanZelawat <namanzelawat@gmail.com>
2020-11-26 14:44:42 +0530

    Copied home page

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,7 +2,6 @@ import * as actionTypes from "./actionTypes";
 
 function reducer(state, action)
 {
-	console.log(action);
 	switch (action.type) {
 	    case actionTypes.TEST:
 	      return { ...state, test: action.payload };
