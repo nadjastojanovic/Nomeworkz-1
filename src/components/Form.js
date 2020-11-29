92133cdccfddc63808f5ec3b3a12f9513d16b741
NaMN-0 <naman1474@gmail.com>
2020-11-29 19:21:51 +0530

    form and hire page connected

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,24 +7,24 @@ function Form()
 	}
 	return(
 		<>
-		<div class="container-fluid">
-				<div class="row">
+		<div className="container-fluid">
+				<div className="row">
 					<div className="col-lg-2 col-md-0 col-sm-0"></div>
-					<div class="col-lg-8 col-md-12 col-sm-12">
-						<div class="card card-inverse p-3">
-							<div class="card-block">
-								<div class="row p-3">
-									<div class="col-md-8 col-sm-8">
-										<h2 class="card-title">Name: Nome Name</h2>
-										<p class="card-text"><strong>Overall Rating: </strong> 
-											<i class="icofont-star"></i>
-											<i class="icofont-star"></i>
-											<i class="icofont-star"></i>
+					<div className="col-lg-8 col-md-12 col-sm-12">
+						<div className="card card-inverse p-3">
+							<div className="card-block">
+								<div className="row p-3">
+									<div className="col-md-8 col-sm-8">
+										<h2 className="card-title">Name: Nome Name</h2>
+										<p className="card-text"><strong>Overall Rating: </strong> 
+											<i className="icofont-star"></i>
+											<i className="icofont-star"></i>
+											<i className="icofont-star"></i>
 										</p>
 										<p><strong>Service Offered: </strong>
-											<span class="badge bg-primary mx-1">Paper</span> 
-											<span class="badge bg-info mx-1">PPT</span>
-											<span class="badge bg-warning mx-1">Resume</span>
+											<span className="badge bg-primary mx-1">Paper</span> 
+											<span className="badge bg-info mx-1">PPT</span>
+											<span className="badge bg-warning mx-1">Resume</span>
 										</p>
 										<p><strong>Info: </strong>
 											I am engineering student studying MnC. I am in third year.
@@ -32,20 +32,20 @@ function Form()
 										</p>
 										<hr/>
 									</div>
-									<div class="col-md-4 col-sm-4 text-center">
-										<img class="btn-md" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK9gqFKRn28xKHD1CAbEevdzsLmsv5yQkGnQ&usqp=CAU" alt="" style={imgStyle}/>
+									<div className="col-md-4 col-sm-4 text-center">
+										<img className="btn-md" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK9gqFKRn28xKHD1CAbEevdzsLmsv5yQkGnQ&usqp=CAU" alt="" style={imgStyle}/>
 									</div>
 
-									<form class="ml-3">
-										<div class="form-group">
+									<form className="ml-3">
+										<div className="form-group">
 											<label for="exampleFormControlFile1">Upload Image and Reference Files (10mb Max)</label>
-											<input type="file" class="form-control-file" id="exampleFormControlFile1" />
+											<input type="file" className="form-control-file" id="exampleFormControlFile1" />
 											<p>If you have larger files then upload files on google drive and share the link in description.</p>
 										</div>
 										<hr/>
-										<div class="form-group">
+										<div className="form-group">
 											<label for="exampleFormControlSelect1">Homework Level</label>
-											<select class="form-control" id="exampleFormControlSelect1">
+											<select className="form-control" id="exampleFormControlSelect1">
 											<option>High School</option>
 											<option>2</option>
 											<option>3</option>
@@ -53,9 +53,9 @@ function Form()
 											<option>5</option>
 											</select>
 										</div>
-										<div class="form-group">
+										<div className="form-group">
 											<label for="exampleFormControlSelect1">Number of Pages (One page will have 500 words approx.)</label>
-											<select class="form-control" id="exampleFormControlSelect1">
+											<select className="form-control" id="exampleFormControlSelect1">
 											<option>1-5</option>
 											<option>5-10</option>
 											<option>10-15</option>
@@ -64,22 +64,22 @@ function Form()
 											</select>
 										</div>
 										<hr/>
-										<div class="form-group">
+										<div className="form-group">
 											<label for="exampleFormControlTextarea1">Description about Homework</label>
-											<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+											<textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
 										</div>
-										<div class="form-group">
+										<div className="form-group">
 											<label for="exampleFormControlTextarea1">Special Notes</label>
-											<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+											<textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
 										</div>
 										<hr/>
-										<div class="form-group">
+										<div className="form-group">
 											<label for="example-date-input">Choose Date</label>
-											<div class="col-10">
-												<input class="form-control" type="date" value="2011-08-19" id="example-date-input"/>
+											<div className="col-10">
+												<input className="form-control" type="date" value="2011-08-19" id="example-date-input"/>
 											</div>
 										</div>
-										<button type="submit" class="btn btn-primary">Calculate Price</button>
+										<button type="submit" className="btn btn-primary">Calculate Price</button>
 									</form>
 								</div>
 							</div>
@@ -88,7 +88,7 @@ function Form()
 				</div> 
 			</div>
 		</>
-		);
+	);
 }
 
 export default Form;
--- src/components/Hire.js
+++ src/components/Hire.js
@@ -1,70 +1,58 @@
 import { Nav, Navbar } from "react-bootstrap";
 
+const nomesArray = [
+	{
+		id : 1,
+		name : "Bill Gates",
+		rating : 1,
+		description : "Some brief details abot the nome. Some brief details abot the nome. Some brief details abot the nome."
+	},
+	{
+		id : 2,
+		name : "Bill Gates",
+		rating : 2,
+		description : "Some brief details abot the nome. Some brief details abot the nome. Some brief details abot the nome."
+	},
+	{
+		id : 3,
+		name : "Bill Gates",
+		rating : 3,
+		description : "Some brief details abot the nome. Some brief details abot the nome. Some brief details abot the nome."
+	},
+	{
+		id : 4,
+		name : "Bill Gates",
+		rating : 4,
+		description : "Some brief details abot the nome. Some brief details abot the nome. Some brief details abot the nome."
+	}
+]
+
 function Hire()
 {
 	return(
 		<>
 		<div className="row">
-			<div className="text-center col-lg-3 col-md-6 col-sm-1 p-5">
-				<div class="card">
-					<img class="card-img-top p-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK9gqFKRn28xKHD1CAbEevdzsLmsv5yQkGnQ&usqp=CAU" alt="Card image cap"/>
-					<div class="card-body">
-					<h5 class="card-title">Nome Name</h5>
-					<p class="card-text"><strong>Overall Rating: </strong> 
-						<i class="icofont-star"></i>
-						<i class="icofont-star"></i>
-						<i class="icofont-star"></i>
-					</p>
-					<p class="card-text">Some brief details abot the nome. Some brief details abot the nome. Some brief details abot the nome.</p>
-					<a href="/form" class="btn btn-primary">Check full details</a>
-					</div>
-				</div>
-			</div>
-			<div className="text-center col-lg-3 col-md-6 col-sm-1 p-5">
-				<div class="card">
-					<img class="card-img-top p-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK9gqFKRn28xKHD1CAbEevdzsLmsv5yQkGnQ&usqp=CAU" alt="Card image cap"/>
-					<div class="card-body">
-					<h5 class="card-title">Nome Name</h5>
-					<p class="card-text"><strong>Overall Rating: </strong> 
-						<i class="icofont-star"></i>
-						<i class="icofont-star"></i>
-						<i class="icofont-star"></i>
-					</p>
-					<p class="card-text">Some brief details abot the nome. Some brief details abot the nome. Some brief details abot the nome.</p>
-					<a href="/form" class="btn btn-primary">Check full details</a>
-					</div>
-				</div>
-			</div>
-			<div className="text-center col-lg-3 col-md-6 col-sm-1 p-5">
-				<div class="card">
-					<img class="card-img-top p-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK9gqFKRn28xKHD1CAbEevdzsLmsv5yQkGnQ&usqp=CAU" alt="Card image cap"/>
-					<div class="card-body">
-					<h5 class="card-title">Nome Name</h5>
-					<p class="card-text"><strong>Overall Rating: </strong> 
-						<i class="icofont-star"></i>
-						<i class="icofont-star"></i>
-						<i class="icofont-star"></i>
-					</p>
-					<p class="card-text">Some brief details abot the nome. Some brief details abot the nome. Some brief details abot the nome.</p>
-					<a href="/form" class="btn btn-primary">Check full details</a>
-					</div>
-				</div>
-			</div>
-			<div className="text-center col-lg-3 col-md-6 col-sm-1 p-5">
-				<div class="card">
-					<img class="card-img-top p-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK9gqFKRn28xKHD1CAbEevdzsLmsv5yQkGnQ&usqp=CAU" alt="Card image cap"/>
-					<div class="card-body">
-					<h5 class="card-title">Nome Name</h5>
-					<p class="card-text"><strong>Overall Rating: </strong> 
-						<i class="icofont-star"></i>
-						<i class="icofont-star"></i>
-						<i class="icofont-star"></i>
-					</p>
-					<p class="card-text">Some brief details abot the nome. Some brief details abot the nome. Some brief details abot the nome.</p>
-					<a href="/form" class="btn btn-primary">Check full details</a>
-					</div>
-				</div>
-			</div>
+			{
+				nomesArray.map(nome => {
+					return(					
+						<div className="text-center col-lg-3 col-md-6 col-sm-1 p-5">
+							<div class="card">
+								<img class="card-img-top p-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK9gqFKRn28xKHD1CAbEevdzsLmsv5yQkGnQ&usqp=CAU" alt="Card image cap"/>
+								<div class="card-body">
+								<h5 class="card-title">{nome.name}</h5>
+								<p class="card-text"><strong>Overall Rating: </strong> 
+									{[...Array(nome.rating)].map((x, i) =>
+										<i class="icofont-star"></i>
+									)}
+								</p>
+								<p class="card-text">{nome.description}</p>
+								<a href={"/form?name="+nome.name+"&description="+nome.description+"&rating="+nome.rating} class="btn btn-primary">Check full details</a>
+								</div>
+							</div>
+						</div>
+					)					
+				})
+			}
 		</div>	
 		</>
 		);
