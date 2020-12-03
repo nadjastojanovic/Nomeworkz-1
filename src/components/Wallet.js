80d24e42a08c1b84472a400b0cff2906d652e82f
NaMN-0 <naman1474@gmail.com>
2020-12-03 21:56:44 +0530

    wallet

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,12 +1,81 @@
 import StripeCheckout from "react-stripe-checkout";
+import { useState } from "react"
 
-function handleToken(token)
+const stripe = require("stripe")("sk_test_51HrQ9WISRuDB0X7oRPe1lkwQzgiLm2PgBy1f2uvlZmRhObzqdwdX7SW1LohcBF8MiJZZ2JZvpPthSnaJEqwQPj7o00e0oeJzvB");
+
+async function handleToken(amount, token)
 {
-	console.log(token);
+	console.log(`token : ${token}`);
+	let status;
+	try{
+		console.log("start");
+		const customer = await
+		stripe.customers.create({
+			email: token.email,
+			source: token.id
+		});
+		console.log("here1");
+		const idempotency_key = 123;
+		const charge = await stripe.charges.create(
+			{
+				amount: amount,
+				currency: "usd",
+				customer: customer.id,
+				receipt_email: token.email,
+				description: `Paid an amount of ${amount}.`,
+			},
+			{
+				idempotency_key
+			}
+		);
+		console.log("here2");
+		status = "success";
+		console.log(status);
+	} 
+	catch(error){
+		console.log(`error : ${error}`);
+		status = "failure";
+	}
 }
+
 
 function Wallet()
 {
+	const [amount, setAmount] = useState(-1);
+	const [error, setError] = useState(1);
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+		let tmp = e.target.amountAdded.value;
+		tmp*=100;
+		console.log(tmp);
+		if(tmp>0){
+			setAmount(tmp);
+			setError(0);
+		}
+		else{
+			setAmount(-1);
+			setError(1);
+		}
+	}
+	let payDiv = "";
+	if(error===1){
+		payDiv = <></>
+	}
+	else{
+		payDiv = 
+		<>
+		  <div class="card-body">
+		    <StripeCheckout 
+		    	stripeKey="pk_test_51HrQ9WISRuDB0X7oHBljiIAhHuJvnYU1lj1oVtbFxpUiQhEmWWaID1eGVi0jbvkVIWpse7D9cVk7Kdk4McfyvtD300sSnLCMU6"
+		    	token={handleToken}
+		    	email={localStorage.email}
+		    	allowRememberMe={false}
+		    	amount={amount}
+		    />
+		  </div>
+		</>
+	}
 	return(
 		<div class="card p-5 m-5">
 		  <div class="card-body">
@@ -17,14 +86,14 @@ function Wallet()
 		    <li class="list-group-item">Current Balance :- {localStorage.wallet}</li>
 		  </ul>
 		  <div class="card-body">
-		    <StripeCheckout 
-		    	stripeKey="pk_test_51HrQ9WISRuDB0X7oHBljiIAhHuJvnYU1lj1oVtbFxpUiQhEmWWaID1eGVi0jbvkVIWpse7D9cVk7Kdk4McfyvtD300sSnLCMU6"
-		    	token={handleToken}
-		    	email={localStorage.email}
-		    	allowRememberMe={false}
-		    	amount={20}
-		    />
+		    <h5 class="card-title">Add amount</h5>
+			<form onSubmit={(e) => submitHandler(e)}>
+				<label for="amountAdded">Enter the amount to be added to wallet.</label>
+				<input name="amountAdded" type="number" className="amountAdded mx-2" id="amountAdded"/>
+				<button action = "submit" className="btn btn-primary">Proceed</button>
+			</form>
 		  </div>
+		  {payDiv}
 		</div>
 		);
 }
